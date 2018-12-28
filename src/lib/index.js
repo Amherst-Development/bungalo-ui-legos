86b2566362d68a047d6afa19d04a9d7d6e5efdd4
Nick Bertrand <nbertrand@bungalohomes.com>
2018-12-28 13:55:05 -0600

    saving some changed files I forgot to save

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
 export { default as Button } from './components/Button';
-=======
-import Button from './Button';
-
-export { Button };
->>>>>>> 9a78677daac7bddb0c66e39530f13b7b1bee7dec
