e4b4d9ee8e2ac1c79031cefeb53c0d6b578498e4
Nick Bertrand <nbertrand@bungalohomes.com>
2018-12-28 15:23:56 -0600

    Moving colors into lib so the buttons and other things can use them

--- src/lib/components/Button/Button.jsx
+++ src/lib/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { grey, blue, orange } from '../../../colors'
+import { grey, blue, orange } from '../../colors'
 
 export class Button extends Component {
   render() {
