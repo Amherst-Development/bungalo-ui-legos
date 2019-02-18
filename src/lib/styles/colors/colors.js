7bb2c9590f5239653a741a47f37309d30ba50142
Nick Bertrand <nbertrand@bungalohomes.com>
2019-02-18 16:24:38 -0600

    Creating colors.js and changing all jsx files ot use it instead of the scss file

---
+++ src/lib/styles/colors/colors.js
@@ -0,0 +1,19 @@
+const colors = {
+  white: '#FFFFFF',
+  black: '#282A29',
+  orange: '#E35F2E',
+  red: '#FF5516',
+  grey: '#5E605F',
+  greyTint: '#EBEBEB',
+  greyLight: '#FCFCFC',
+  blue: '#7FCAF6',
+  blueTint: '#DDEDF2',
+  brass: '#B5936B',
+  brassTint: '#F1EBD9',
+  lemon: '#F9E547',
+  lemonTint: '#FDF9ED',
+  apricot: '#EDA146',
+  blueberry: '#004976',
+}
+
+export default colors
