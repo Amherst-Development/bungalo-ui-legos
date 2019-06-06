991ed4e59fce4fa03556195f0b88e7c8cd50a4c0
Nick Bertrand <nbertrand@bungalohomes.com>
2019-06-06 11:01:43 -0500

    Adding new color gradients

--- src/docsHelpers/ColorSwatch.jsx
+++ src/docsHelpers/ColorSwatch.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import colors from '../lib/styles/colors/colors.scss'
+import colors from '../lib/styles/colors/colors'
 
 
 export default class ColorSwatch extends Component {
