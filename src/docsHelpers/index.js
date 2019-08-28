3263e1a41aa35f309805ef6863625080503032e0
Nick Bertrand <nbertrand@bungalohomes.com>
2019-08-28 15:49:20 -0500

    Adding question icon and reformatting icons page a bit

--- src/docsHelpers/index.js
+++ src/docsHelpers/index.js
@@ -1,3 +1,4 @@
 export { default as MarginWrapper } from './MarginWrapper'
 export { default as ColorSwatch } from './ColorSwatch'
 export { default as ColorsWrapper } from './ColorsWrapper'
+export { default as IconWrapper } from './IconWrapper'
