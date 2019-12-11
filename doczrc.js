3f29368e503521b40a1b4baafea6ac72f3c3cf2c
Nick Bertrand <nbertrand@bungalohomes.com>
2019-12-11 13:10:40 -0600

    General cleanup

--- doczrc.js
+++ doczrc.js
@@ -1,28 +1,3 @@
-// export default {
-//   modifyBundlerConfig: (config) => {
-//     config.module.rules.push({
-//       test: /\.scss$/,
-//       use: ['style-loader', 'css-loader', 'sass-loader'],
-//     })
-//     return config
-//   },
-// }
-
-// exports.onCreateWebpackConfig = ({
-// stage, getConfig, rules, loaders, actions,
-// }) => {
-//   actions.setWebpackConfig({
-//     module: {
-//       rules: [
-//         {
-//           test: /\.scss$/,
-//           use: [loaders.style(), loaders.css()]
-//         },
-//       ],
-//     },
-//   })
-// }
-
 export default {
   title: 'Bungalo Ui Legos',
   menu: ['Components'],
