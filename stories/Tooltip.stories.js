5ffb29b57a4cab248513f8d89b233ddc34d85efd
Nick Bertrand <nbertrand@bungalohomes.com>
2020-02-07 13:54:36 -0600

    Cleanup Welcome page and Tooltip page

--- stories/Tooltip.stories.js
+++ stories/Tooltip.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { addParameters } from '@storybook/react'
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
+import colors from '../src/lib/styles/colors/colors.js'
 import { Tooltip, InfoIcon } from '../src/lib'
 
 addParameters({
@@ -15,45 +16,45 @@ export default {
   component: Tooltip,
 }
 export const AllTooltips = () => (
-  <div style={ { margin: '32px 128px', padding: '8px' } }>
+  <div style={ { margin: '32px 128px', padding: '8px', color: colors.grey700 } }>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-top' text='This is a top tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-bottom' text='This is a bottom tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-left' text='This is a left tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-right' text='This is a right tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-top-left' text='This is a top left tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-bottom-left' text='This is a bottom left tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-top-right' text='This is a top right tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
     <div style={ { height: '42px', padding: '8px' } }>
       <Tooltip direction='tooltip-bottom-right' text='This is a bottom right tooltip'>
-        <InfoIcon color='#000000' size='36px' />
+        <InfoIcon size='36px' />
       </Tooltip>
     </div>
   </div>
