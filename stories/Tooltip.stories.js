f4f6c997f1e2c6406b098e644571119d815ee4f3
Nick Bertrand <nbertrand@bungalohomes.com>
2020-02-10 14:35:36 -0600

    Cleanup Tooltips

--- stories/Tooltip.stories.js
+++ stories/Tooltip.stories.js
@@ -16,12 +16,6 @@ export default {
   component: Tooltip,
 }
 
-export const TopTooltip = () => (
-  <Tooltip direction='tooltip-top' text='This is a top tooltip'>
-    <InfoIcon size='36px' />
-  </Tooltip>
-)
-
 export const AllTooltips = () => (
   <div style={ { margin: '32px 128px', padding: '8px', color: colors.grey700 } }>
     <div style={ { height: '42px', padding: '8px' } }>
@@ -64,5 +58,69 @@ export const AllTooltips = () => (
         <InfoIcon size='36px' />
       </Tooltip>
     </div>
+  </div>
+)
+
+export const TopTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-top' text='This is a top tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const BottomTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-bottom' text='This is a bottom tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const LeftTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-left' text='This is a left tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const RightTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-right' text='This is a right tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const TopLeftTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-top-left' text='This is a top-left tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const TopRightTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-top-right' text='This is a top-right tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const BottomLeftTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-bottom-left' text='This is a bottom-left tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
+  </div>
+)
+
+export const BottomRightTooltip = () => (
+  <div style={ { margin: '16px 32px 64px', padding: '32px', color: colors.grey700 } }>
+    <Tooltip direction='tooltip-bottom-right' text='This is a bottom-right tooltip'>
+      <InfoIcon size='36px' />
+    </Tooltip>
   </div>
 )
